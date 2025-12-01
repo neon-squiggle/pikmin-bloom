2a4836cd935f847882ea7de19e6358a0297c972c
Ada Chen <ada.chen@berkeley.edu>
2025-11-30 20:07:37 -0800

    save for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import { CssBaseline } from "@mui/material";
+import Container from "@mui/material/Container";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import Calculator from "./MushCalculator";
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import "./App.css";
@@ -8,8 +11,12 @@ const darkTheme = createTheme({ palette: { mode: "dark" } });
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
-      <CssBaseline />
-      <Calculator />
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <Container>
+          <CssBaseline />
+          <Calculator />
+        </Container>
+      </LocalizationProvider>
     </ThemeProvider>
   );
 }
--- src/MushCalculator.tsx
+++ src/MushCalculator.tsx
@@ -1,58 +1,161 @@
 import React, { useState } from "react";
-import Card from "@mui/material/Card";
-import TextField from "@mui/material/TextField";
-import Autocomplete from "@mui/material/Autocomplete";
+import {
+  CardContent,
+  CardHeader,
+  Typography,
+  Divider,
+  Card,
+  TextField,
+  Autocomplete,
+  ToggleButtonGroup,
+  Box,
+} from "@mui/material";
+import NumberSpinner from "./NumberSpinner";
+import dayjs, { Dayjs } from "dayjs";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 
-const mushrooms = {
-  smallRed: 87400,
-  smallYellow: 84200,
-  smallBlue: 84200,
-  smallPurple: 93900,
-  smallWhite: 81000,
-  smallPink: 81000,
-  smallGray: 90700,
-  smallIceBlue: 81000,
-  normalRed: 670600,
-  normalYellow: 645800,
-  normalBlue: 645800,
-  normalPurple: 720300,
-  normalWhite: 621000,
-  normalPink: 621000,
-  normalGray: 695500,
-  normaIceBlue: 621000,
-  normalFire: 3850200,
-  normalWater: 3816700,
-  normalCrystal: 3883600,
-  normalElectric: 3816700,
-  normalPoisonous: 3783200,
-  largeRed: 2916000,
-  largeYellow: 2808000,
-  largeBlue: 2808000,
-  largePurple: 3132000,
-  largeWhite: 2700000,
-  largePink: 2700000,
-  largeGray: 3024000,
-  largeIceBlue: 2700000,
-  largeFire: 13662000,
-  largeWater: 13543200,
-  largeCrystal: 13780800,
-  largeElectric: 13543200,
-  largePoisonous: 13424400,
-};
+interface Mushroom {
+  label: string;
+  value: number;
+}
+
+const mushrooms: Mushroom[] = [
+  { label: "Small Red", value: 87400 },
+  { label: "Small Yellow", value: 84200 },
+  { label: "Small Blue", value: 84200 },
+  { label: "Small Purple", value: 93900 },
+  { label: "Small White", value: 81000 },
+  { label: "Small Pink", value: 81000 },
+  { label: "Small Gray", value: 90700 },
+  { label: "Small Ice Blue", value: 81000 },
+  { label: "Normal Red", value: 670600 },
+  { label: "Normal Yellow", value: 645800 },
+  { label: "Normal Blue", value: 645800 },
+  { label: "Normal Purple", value: 720300 },
+  { label: "Normal White", value: 621000 },
+  { label: "Normal Pink", value: 621000 },
+  { label: "Normal Gray", value: 695500 },
+  { label: "Normal Ice Blue", value: 621000 },
+  { label: "Normal Fire", value: 3850200 },
+  { label: "Normal Water", value: 3816700 },
+  { label: "Normal Crystal", value: 3883600 },
+  { label: "Normal Electric", value: 3816700 },
+  { label: "Normal Poisonous", value: 3783200 },
+  { label: "Large Red", value: 2916000 },
+  { label: "Large Yellow", value: 2808000 },
+  { label: "Large Blue", value: 2808000 },
+  { label: "Large Purple", value: 3132000 },
+  { label: "Large White", value: 2700000 },
+  { label: "Large Pink", value: 2700000 },
+  { label: "Large Gray", value: 3024000 },
+  { label: "Large Ice Blue", value: 2700000 },
+  { label: "Large Fire", value: 13662000 },
+  { label: "Large Water", value: 13543200 },
+  { label: "Large Crystal", value: 13780800 },
+  { label: "Large Electric", value: 13543200 },
+  { label: "Large Poisonous", value: 13424400 },
+];
+
+const higherBaseAp = ["Yellow", "Purple", "Pink", "Ice Blue"];
 
 const Calculator = () => {
-  const [ap, setAp] = useState<number>(0);
-  const [health, setHealth] = useState<number>(0);
+  const [mush, setMush] = useState<Mushroom | null>(null);
+  const [health, setHealth] = useState<number>(1);
+  const [pikminAp, setPikminAp] = useState<number>(2);
+  const [apMin, setApMin] = useState<number>(2);
+  const [isEstimate, setIsEstimate] = useState<boolean>(true);
+  const [timeLeft, setTimeLeft] = useState<Dayjs | null>(null);
+
+  const handleMushChange = (mush: Mushroom | null) => {
+    setMush(mush);
+    const mushHealth = mush?.value ?? 1;
+    setHealth(mushHealth);
+    if (pikminAp) {
+      setTimeLeft(calculateTime(mushHealth, pikminAp));
+    }
+    if (higherBaseAp.some((color) => mush?.label.includes(color))) {
+      setApMin(5);
+      setPikminAp(5);
+    } else {
+      setApMin(2);
+      setPikminAp(2);
+    }
+  };
+
+  const handleHealthChange = (value: number | null) => {
+    setMush(null);
+    setHealth(value ?? 1);
+  };
+
+  const handleApChange = (value: number | null) => {
+    const ap = value ?? 1;
+    setPikminAp(ap);
+    if (mush) {
+      setTimeLeft(calculateTime(mush.value, ap));
+    }
+  };
+
+  const calculateTime = (health: number, ap: number) => {
+    const timeEstimate = (health * 100) / ap;
+    return dayjs().add(timeEstimate, "second");
+  };
+
+  const calculateAp = (health: number, time: number) => {
+    const timeEstimate = dayjs(time).diff(dayjs(), "second");
+    return (health * 100) / timeEstimate;
+  };
+
   return (
-    <Card>
-      <Autocomplete
-        disablePortal
-        options={Object.keys(mushrooms)}
-        sx={{ width: 300 }}
-        renderInput={(params) => (
-          <TextField {...params} label="Mushroom Type" />
-        )}
-      />
+    <Card variant="elevation">
+      <CardHeader title="Mushroom calculator" />
+      <CardContent>
+        <Typography
+          gutterBottom
+          variant="body1"
+          sx={{ color: "text.secondary" }}
+        >
+          all three
+        </Typography>
+        <Divider />
+        <Box sx={{ display: "flex", gap: 8, p: 2 }}>
+          <Typography variant="h6">Mushroom</Typography>
+          <Autocomplete
+            disablePortal
+            options={mushrooms}
+            sx={{ width: 300, paddingTop: 2.75 }}
+            onChange={(e: any, mush: Mushroom | null) => handleMushChange(mush)}
+            value={mush}
+            renderInput={(params: any) => (
+              <TextField {...params} label="Mushroom Type" />
+            )}
+          />
+          <NumberSpinner
+            label="Mushroom health"
+            min={1}
+            value={health}
+            onValueChange={handleHealthChange}
+          />
+        </Box>
+        <Divider />
+        <Box sx={{ p: 2, gap: 8, display: "flex" }}>
+          <Typography variant="h6">Attack Power</Typography>
+          <NumberSpinner
+            min={apMin}
+            value={pikminAp}
+            onValueChange={handleApChange}
+          />
+        </Box>
+        <Divider />
+        <Box sx={{ p: 2, gap: 8, display: "flex" }}>
+          <Typography variant="h6">Time</Typography>
+          <DateTimePicker
+            label="Desired End Time"
+            disabled={health === 1}
+            value={timeLeft}
+            onChange={setTimeLeft}
+          />
+        </Box>
+      </CardContent>
     </Card>
   );
 };
